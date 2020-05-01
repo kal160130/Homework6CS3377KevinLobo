900a68b2a4ea3f28d9b6f6259a2ae14db853a917
kal160130 <kal160130@utdallas.edu>
2020-04-30 23:25:01 -0500

    Adding files

---
+++ BinFile.cpp
@@ -0,0 +1,10 @@
+/* 
+   Header.cpp
+   4/30/2020
+   Kevin Lobo
+   kal160130@utdallas.edu
+   CS 3377.501 Spring 2020
+   Version 1.0
+   2020 All rights reserved
+   Header file for Assignment 6 programs
+*/
